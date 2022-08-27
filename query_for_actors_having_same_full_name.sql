9ac6572e11b9545485f243584e3ddd428570d835
Frank Obeng <obengfan@gmail.com>
2022-08-27 09:01:27 +0200

    querry for actors that share same name

--- query_for_actors_having_same_full_name.sql
+++ query_for_actors_having_same_full_name.sql
@@ -2,3 +2,8 @@
 -- actor count is 200 and unique actor count is 199. ----
 --- this question is to help you find that one actor that is repeating ---
 
+SELECT first_name,last_name,
+COUNT(*)
+FROM actor
+GROUP BY first_name, last_name
+HAVING COUNT(*) >1;
